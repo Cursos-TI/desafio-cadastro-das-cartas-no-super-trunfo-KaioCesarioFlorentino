45f33669aee06a91bbbc410efa2271bd96037f0a
KaioCesarioFlorentino <kaiocesario2005@gmail.com>
2025-09-08 11:11:58 +0000

    Declaradas variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,29 @@
 
 int main() {
 
+    //Declarando as variáveis
+
+    char estado[1];
+    char codigo[3];
+    char nomedacidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+
+    //Instruindo o usuário sobre como criar as cartas de acordo com as regras do jogo
+
+    xxxx
+
+    //Adicionando o estado
+ 
+    printf("Digite o Estado: "); 
+    scanf("%c", estado);
+
+    //Adicionando o Código da carta
+
+    printf("Digite o Código da carta: ");
+    scanf("%s", codigo);
 
     return 0;
 }
